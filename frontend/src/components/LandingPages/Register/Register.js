07f3e31b119d98f6eae0e8f5825eaa3d456555fb
Dushyant Agrawal <dushyantagrawal0304@gmail.com>
2023-09-02 11:01:42 +0530

    [MODIFIED] Signup page

--- frontend/src/components/LandingPages/Register/Register.js
+++ frontend/src/components/LandingPages/Register/Register.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react';
 import { Outlet, useNavigate } from 'react-router';
 import styles from './Register.module.css';
+import Img1 from '../Home/Images/bg.webp';
+import Img2 from '../Home/Imgages/bg.png';
 
 function Register() {
   const navigate = useNavigate();
